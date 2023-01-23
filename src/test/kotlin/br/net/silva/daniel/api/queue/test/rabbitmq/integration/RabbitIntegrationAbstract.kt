f2dbd337f7ee8bc581d8f76f6cc499a10b171a84
daniel.cunha <daniel.cunha@redspark.io>
2023-01-23 15:43:25 -0300

    Ajustado Geração de test containers para rabbitmq

--- src/test/kotlin/br/net/silva/daniel/api/queue/test/rabbitmq/integration/RabbitIntegrationAbstract.kt
+++ src/test/kotlin/br/net/silva/daniel/api/queue/test/rabbitmq/integration/RabbitIntegrationAbstract.kt
@@ -1,32 +1,39 @@
 package br.net.silva.daniel.api.queue.test.rabbitmq.integration
 
+import org.junit.runner.RunWith
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.util.TestPropertyValues
+import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringRunner
 import org.testcontainers.containers.RabbitMQContainer
 
+@RunWith(SpringRunner::class)
+@SpringBootTest
+@ContextConfiguration(initializers = [RabbitIntegrationAbstract.Initializer::class])
+@AutoConfigureMockMvc
 abstract class RabbitIntegrationAbstract {
 
-    val rabbitContainer = RabbitMQContainer("rabbitmq:3.8.9-management")
-        .withExposedPorts(5672, 15672)
-        .withQueue("test.queue.api")
-        .withBinding("test.queue.api", "test.exchange.api", "api")
-        .withExchange("test.exchange.api", "direct")
-        .withUser("guest", "guest")
-
-
-    init {
-        rabbitContainer.start()
+    companion object {
+        val rabbitContainer = RabbitMQContainer("rabbitmq:3.8.9-management").apply {
+            withExposedPorts(5672, 15672)
+            withQueue("test.queue.api")
+            withUser("guest", "guest")
+        }
     }
 
-    fun close() {
-        rabbitContainer.stop()
-    }
+    internal class Initializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
+        override fun initialize(configurableApplicationContext: ConfigurableApplicationContext) {
+            rabbitContainer.start()
 
-    fun initialize(configurationApplicationContext: ConfigurableApplicationContext) {
-        val values = TestPropertyValues.of(
-            "spring.rabbitmq.host=" + rabbitContainer.host,
-            "spring.rabbitmq.port=" + rabbitContainer.getMappedPort(5672),
-        )
-        values.applyTo(configurationApplicationContext)
+            TestPropertyValues.of(
+                "spring.rabbitmq.host=${rabbitContainer.host}",
+                "spring.rabbitmq.username=${rabbitContainer.adminUsername}",
+                "spring.rabbitmq.password=${rabbitContainer.adminPassword}",
+                "spring.rabbitmq.port=${rabbitContainer.amqpPort}"
+            ).applyTo(configurableApplicationContext.environment)
+        }
     }
 }
--- src/test/kotlin/br/net/silva/daniel/api/queue/test/rabbitmq/service/QueueServiceIntegrationRabbitTest.kt
+++ src/test/kotlin/br/net/silva/daniel/api/queue/test/rabbitmq/service/QueueServiceIntegrationRabbitTest.kt
@@ -1,5 +1,6 @@
 package br.net.silva.daniel.api.queue.test.rabbitmq.service
 
+import br.net.silva.daniel.api.queue.test.rabbitmq.integration.RabbitIntegrationAbstract
 import br.net.silva.daniel.api.queue.test.rabbitmq.util.JsonConverter
 import br.net.silva.daniel.api.queue.test.rabbitmq.value_object.PeopleMessage
 import com.rabbitmq.client.ConnectionFactory
@@ -7,51 +8,34 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
-import org.testcontainers.containers.RabbitMQContainer
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
 @Tag("integration")
 @SpringBootTest
 class QueueServiceIntegrationRabbitTest(
+    @Autowired private val rabbitTemplate: RabbitTemplate,
     @Value("\${api.exchange}") private val exchange: String,
-    @Value("\${api.identifier}") private val identifier: String
-) {
-
-    @Container
-    val rabbitContainer = RabbitMQContainer("rabbitmq:3-management").also {
-        it.withExposedPorts(5672, 15672)
-        it.withAccessToHost(true)
-        it.withExchange(exchange, "direct")
-        it.withQueue("test.queue.api")
-    }
+    @Value("\${api.identifier}") private val identifier: String,
+    @Value("\${spring.rabbitmq.host}") private val host: String,
+    @Value("\${spring.rabbitmq.port}") private val port: Int,
+): RabbitIntegrationAbstract() {
 
     @Test
     fun `should send message to queue`() {
-        val connectionFactory = CachingConnectionFactory(rabbitContainer.host).apply {
-            username = rabbitContainer.adminUsername
-            setPassword(rabbitContainer.adminPassword)
-            port = rabbitContainer.amqpPort
-        }
 
-        val service = QueueService(RabbitTemplate(connectionFactory), exchange, identifier)
-
-        assertTrue(rabbitContainer.isRunning)
-
-        println(rabbitContainer.getMappedPort(5672))
+        val service = QueueService(rabbitTemplate, exchange, identifier)
         val message = PeopleMessage("Hello", "Daniel", 33)
         val json = JsonConverter.convert(message)
         service.send(json)
 
         val factoryRabbit = ConnectionFactory()
-        factoryRabbit.host = rabbitContainer.host
-        factoryRabbit.port = rabbitContainer.amqpPort
+        factoryRabbit.host = host
+        factoryRabbit.port = port
 
         val connection = factoryRabbit.newConnection()
         val channel = connection.createChannel()
