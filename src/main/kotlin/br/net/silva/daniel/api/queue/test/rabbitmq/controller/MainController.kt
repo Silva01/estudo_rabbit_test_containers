1072e861379b15df92889839873282050d91ad4f
daniel.cunha <daniel.cunha@redspark.io>
2023-01-23 16:36:37 -0300

    Adicionado implementação do endpoint /api/

--- src/main/kotlin/br/net/silva/daniel/api/queue/test/rabbitmq/controller/MainController.kt
+++ src/main/kotlin/br/net/silva/daniel/api/queue/test/rabbitmq/controller/MainController.kt
@@ -1,5 +1,9 @@
 package br.net.silva.daniel.api.queue.test.rabbitmq.controller
 
+import br.net.silva.daniel.api.queue.test.rabbitmq.service.QueueService
+import br.net.silva.daniel.api.queue.test.rabbitmq.util.JsonConverter
+import br.net.silva.daniel.api.queue.test.rabbitmq.value_object.PeopleMessage
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,11 +12,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class MainController {
+class MainController(@Autowired private val service: QueueService) {
 
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
     fun index() {
-        println("Teste")
+        val message = PeopleMessage("Hello", "Daniel", 33)
+        service.send(JsonConverter.convert(message))
     }
 }
